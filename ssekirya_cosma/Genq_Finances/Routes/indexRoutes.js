3f446209e533ecd47a749745fd3d0065b0cf1c35
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-04-28 13:33:35 +0300

    added select options

--- ssekirya_cosma/Genq_Finances/Routes/indexRoutes.js
+++ ssekirya_cosma/Genq_Finances/Routes/indexRoutes.js
@@ -15,7 +15,7 @@ router.post('/index', async(req,res)=>{
     try{
         const register = new Customer(req.body);
         await register.save()
-        res.redirect('/index')//redirect to a path, render a file
+        res.redirect('/')//redirect to a path, render a file
         console.log(req.body)
     }
     catch(err){
@@ -23,59 +23,5 @@ router.post('/index', async(req,res)=>{
         //res.status(400).render("/")
     }
 })
-
-//read data via students.pug
-router.get("/students", async(req,res)=>{
-    try{
-        let items = await Register.find();//go to our collection 'Register', find every record and store them in a variable items.       
-        //console.log(items)
-        //sum up fees
-        let fees = await Register.aggregate([{
-            "$group":{_id: "$all",
-            totalFees: {$sum: "$fees"}
-        }
-        }])
-        res.render("students",{students:items, total:fees[0]}) 
-    }
-    catch(err){
-        console.log(err)
-        res.send('Failed to retrieve student details')
-    }
-  });
-
-//delete functionality
-router.post('/students/delete', async(req,res)=>{
-    try{
-        await Register.deleteOne({_id:req.body.id});
-        res.redirect('back') //this line keeps us on our operating page
-    }
-    catch(err){
-        console.log(err)
-    }
-});
-
-//update route
-router.get("/studentEdit/:id", async(req,res)=>{
-    try{
-        const item = await Register.findOne({_id:req.params.id}); //filtering
-        res.render("studentEdit", {student:item})
-    }
-    catch(err){
-        res.send("Could not find student");
-        console.log(err)
-    }
-})
-
-router.post("/studentEdit", async(req,res)=>{
-    try{
-        await Register.findOneAndUpdate({_id:req.query.id},req.body)
-        res.redirect("/students")
-    }
-    catch(err){
-        res.send("Failed to update student")
-        console.log(err)
-    }
-})
-
 
 module.exports = router
